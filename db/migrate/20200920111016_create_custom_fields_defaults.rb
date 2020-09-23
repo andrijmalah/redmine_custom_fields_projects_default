0624ee7ecd1553aa547b67124f41b41cfe166fdd
andrijmalah <andrijmalah@gmail.com>
2020-09-23 11:48:47 +0300

    migrate in 4.2 rails

--- db/migrate/20200920111016_create_custom_fields_defaults.rb
+++ db/migrate/20200920111016_create_custom_fields_defaults.rb
@@ -1,4 +1,4 @@
-class CreateCustomFieldsDefaults < ActiveRecord::Migration[5.2]
+class CreateCustomFieldsDefaults < ActiveRecord::Migration[4.2]
   def change
     create_table :custom_fields_defaults do |t|
       t.references :project, type: :integer
